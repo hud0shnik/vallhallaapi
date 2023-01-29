cc5350d71413a2a292c2a0ea0efac0d51c546b25
Danila Egorov <c4nsero@mail.ru>
2023-01-29 18:32:34 +0300

    add search.go

---
+++ code/api/search.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+type CocktailResponse struct {
+	Success  bool     `json:"success"`
+	Error    string   `json:"error"`
+	Cocktail Cocktail `json:"cocktail"`
+}
+
+type Cocktail struct {
+	Id            int    `json:"id" db:"id"`
+	Name          string `json:"name" binding:"required"`
+	Alcoholic     string `json:"alcoholic" binding:"required"`
+	Ice           string `json:"ice" binding:"required"`
+	Flavor        string `json:"flavor" binding:"required"`
+	Price         int    `json:"price" binding:"required"`
+	PrimaryType   string `json:"primary_type" binding:"required"`
+	SecondaryType string `json:"secondary_type" binding:"required"`
+}
+
