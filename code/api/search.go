c082743e5567d9feb97f0914e2e9eced15e4a6e7
Danila Egorov <c4nsero@mail.ru>
2023-01-30 15:57:58 +0300

    update search: big update
    Обновил структуры
    Добавил возможность роута без параметров
    Поменял Get на Select
    Добавил проверку на количество коктейлей
    Убрал проверку на айдишник
    Добавил обработчик параметров

--- code/api/search.go
+++ code/api/search.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-	"strconv"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
@@ -15,33 +16,71 @@ import (
 
 // Структура респонса
 type CocktailResponse struct {
-	Success  bool     `json:"success"`
-	Error    string   `json:"error"`
-	Cocktail Cocktail `json:"cocktail"`
+	Success   bool       `json:"success"`
+	Error     string     `json:"error"`
+	Cocktails []Cocktail `json:"cocktail"`
 }
 
 // Структура коктейля
 type Cocktail struct {
-	Id             int    `json:"id" db:"id"`
-	Name           string `json:"name" binding:"required"`
-	Alcoholic      string `json:"alcoholic" binding:"required"`
-	Ice            string `json:"ice" binding:"required"`
-	Flavor         string `json:"flavor" binding:"required"`
-	Price          int    `json:"price" binding:"required"`
-	Primary_Type   string `json:"primary_type" binding:"required"`
-	Secondary_Type string `json:"secondary_type" binding:"required"`
+	//Id             int    `json:"id" db:"id"`
+	Name           string `json:"name"`
+	Alcoholic      string `json:"alcoholic"`
+	Ice            string `json:"ice"`
+	Flavor         string `json:"flavor"`
+	Price          int    `json:"price"`
+	Primary_Type   string `json:"primary_type"`
+	Secondary_Type string `json:"secondary_type"`
+	Recept         string `json:"recept"`
 }
 
 // Функция получения информации о коктейле
-func GetCocktail(db *sqlx.DB, id int) CocktailResponse {
+func GetCocktail(db *sqlx.DB, values url.Values) CocktailResponse {
+
+	query := "SELECT name, price, alcoholic, ice, flavor, primary_type, secondary_type, recept FROM cocktails"
+	parameters := []string{}
+
+	if values.Has("ice") {
+		parameters = append(parameters, "ice = '"+values.Get("ice")+"'")
+	}
+
+	if values.Has("name") {
+		parameters = append(parameters, "name = '"+values.Get("name")+"'")
+	}
+
+	if values.Has("price") {
+		parameters = append(parameters, "price = "+values.Get("price"))
+	}
+
+	if values.Has("alcoholic") {
+		parameters = append(parameters, "alcoholic = '"+values.Get("alcoholic")+"'")
+	}
+
+	if values.Has("flavor") {
+		parameters = append(parameters, "flavor = '"+values.Get("flavor")+"'")
+	}
+
+	if values.Has("primary_type") {
+		parameters = append(parameters, "primary_type = '"+values.Get("primary_type")+"'")
+	}
+
+	if values.Has("secondary_type") {
+		parameters = append(parameters, "secondary_type = '"+values.Get("secondary_type")+"'")
+	}
+
+	if len(parameters) > 0 {
+		query += " WHERE " + strings.ReplaceAll(strings.ReplaceAll(strings.Join(parameters, " AND "), "no", "No"), "yes", "Yes")
+	}
 
 	// Инициализация результата
 	var result CocktailResponse
 
-	// Получение данных и проверка респонса
-	err := db.Get(&result.Cocktail, "SELECT * FROM cocktails WHERE id = $1", id)
+	// Получение и проверка данных
+	err := db.Select(&result.Cocktails, query)
 	if err != nil {
 		result.Error = err.Error()
+	} else if len(result.Cocktails) == 0 {
+		result.Error = "Cocktails not found"
 	} else {
 		result.Success = true
 	}
@@ -53,15 +92,6 @@ func GetCocktail(db *sqlx.DB, id int) CocktailResponse {
 
 // Роут "/search"
 func Search(w http.ResponseWriter, r *http.Request) {
-
-	// Если параметра нет, отправка ошибки
-	if !r.URL.Query().Has("id") {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Получение параметра id из реквеста
-	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
 
 	// Передача в заголовок респонса типа данных
 	w.Header().Set("Content-Type", "application/json")
@@ -90,7 +120,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Получение статистики, форматирование и отправка
-	jsonResp, err := json.Marshal(GetCocktail(db, id))
+	jsonResp, err := json.Marshal(GetCocktail(db, r.URL.Query()))
 	if err != nil {
 		fmt.Print("Error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
