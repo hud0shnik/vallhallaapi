fdbf57f2360a16c276948adffc908a1574114b51
Danila Egorov <c4nsero@mail.ru>
2023-06-07 16:26:55 +0300

    update postgres: add test
    Добавил тесты для postgres

---
+++ postgres/postgres_test.go
@@ -0,0 +1,48 @@
+package postgres_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hud0shnik/VallHalla-api/postgres"
+	"github.com/joho/godotenv"
+)
+
+// Проверка наличия всех переменных окружения
+func TestEnv(t *testing.T) {
+
+	// Загрузка переменных окружения
+	godotenv.Load("../.env")
+
+	// Проверка значений
+	if os.Getenv("DB_HOST") == "" {
+		t.Error("DB_HOST not found")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		t.Error("DB_PORT not found")
+	}
+	if os.Getenv("DB_USERNAME") == "" {
+		t.Error("DB_USERNAME not found")
+	}
+	if os.Getenv("DB_NAME") == "" {
+		t.Error("DB_NAME not found")
+	}
+	if os.Getenv("DB_PASSWORD") == "" {
+		t.Error("DB_PASSWORD not found")
+	}
+
+}
+
+// Проверка соединения с базой данных
+func TestConnectDB(t *testing.T) {
+
+	// Загрузка переменных окружения
+	godotenv.Load("../.env")
+
+	// Подключение к базе данных
+	_, err := postgres.ConnectDB()
+	if err != nil {
+		t.Errorf("Can't reach Postgres: %s", err.Error())
+	}
+
+}
